4bc6a6b3fd8c6054fb49510f6b8273585431989e
antuk21 <74247098+antuk21@users.noreply.github.com>
2026-07-02 15:07:43 +0100

    Update install-qbcore.ps1

--- install-qbcore.ps1
+++ install-qbcore.ps1
@@ -1,19 +1,43 @@
-Write-Host ""
-Write-Host "========================================="
-Write-Host " TCAdmin QBCore Installer"
-Write-Host "========================================="
+Clear-Host
+
+Write-Host "============================================="
+Write-Host "      TCAdmin QBCore Installer v1.0"
+Write-Host "============================================="
 Write-Host ""
 
-$Git = Get-Command git -ErrorAction SilentlyContinue
+# Check Git
+$git = Get-Command git -ErrorAction SilentlyContinue
 
-if ($null -eq $Git)
+if ($null -eq $git)
 {
-    Write-Host "Git is not installed!" -ForegroundColor Red
+    Write-Host "ERROR: Git is not installed!" -ForegroundColor Red
+    pause
     exit
 }
 
-Write-Host "Git Found:"
+Write-Host "[OK] Git Found"
 git --version
 
 Write-Host ""
-Write-Host "Installer Ready"
+
+# Create folders
+$Root = ".\server-data"
+$Resources = "$Root\resources"
+$QB = "$Resources\[qb]"
+
+New-Item -ItemType Directory -Force $QB | Out-Null
+
+Write-Host "[OK] Folder Structure Created"
+
+Write-Host ""
+
+# Clone qb-core
+git clone https://github.com/qbcore-framework/qb-core.git "$QB\qb-core"
+
+Write-Host ""
+
+Write-Host "============================================="
+Write-Host "Finished!"
+Write-Host "============================================="
+
+pause
