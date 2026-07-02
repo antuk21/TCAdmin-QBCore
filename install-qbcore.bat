cd2da85ab98ce1710bd62dd17af539a955b79e7e
antuk21 <74247098+antuk21@users.noreply.github.com>
2026-07-02 14:59:49 +0100

    Create install-qbcore.bat

---
+++ install-qbcore.bat
@@ -0,0 +1,11 @@
+@echo off
+title TCAdmin QBCore Installer
+
+echo =====================================
+echo     TCAdmin QBCore Installer
+echo =====================================
+echo.
+
+powershell -ExecutionPolicy Bypass -File install-qbcore.ps1
+
+pause
